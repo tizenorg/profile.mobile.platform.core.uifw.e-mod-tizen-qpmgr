08fd6739be21584b64cbe6e040ddaf106bd0283f
Seunghun Lee <shiin.lee@samsung.com>
2016-01-12 21:48:36 +0900

    Modification for enlightenment version upgrade 0.20.0

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -1,8 +1,8 @@
 #include "e_mod_main.h"
 
-EAPI E_Module_Api e_modapi = { E_MODULE_API_VERSION, "Quickpanel-Manager" };
+E_API E_Module_Api e_modapi = { E_MODULE_API_VERSION, "Quickpanel-Manager" };
 
-EAPI void *
+E_API void *
 e_modapi_init(E_Module *m)
 {
    printf("LOAD %s MODULE\n", e_modapi.name);
@@ -14,7 +14,7 @@ e_modapi_init(E_Module *m)
    return m;
 }
 
-EAPI int
+E_API int
 e_modapi_shutdown(E_Module *m EINA_UNUSED)
 {
    e_mod_qpmgr_quickpanel_server_shutdown();
@@ -24,7 +24,7 @@ e_modapi_shutdown(E_Module *m EINA_UNUSED)
    return 1;
 }
 
-EAPI int
+E_API int
 e_modapi_save(E_Module *m EINA_UNUSED)
 {
    /* Save something to be kept */
